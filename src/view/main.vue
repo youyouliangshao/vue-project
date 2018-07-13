1a83be6d7fc28ca2e0865a5b045a2d26fd43a97c
cuiyouliang <cuiyouliang@limikeji.com>
2018-07-13 16:49:47 +0800

    demo提交

--- src/view/main.vue
+++ src/view/main.vue
@@ -1,22 +1,9 @@
 <template>
-  <div class="risk-home">
-    <!--<div v-if="mainTabs.length > 0">
-      <el-tabs v-model="mainTabsVal" type="card" closable @tab-remove="removeTab">
-        <el-tab-pane
-          v-for="(item, index) in mainTabs"
-          :key="item.name"
-          :label="item.title"
-          :name="item.name"
-        >
-          <component :is="item.content"></component>
-        </el-tab-pane>
-      </el-tabs>
-    </div>-->
+  <div class="home">
     <div>
-      <p class="welcome-message">欢迎使用汇融风控引擎系统</p>
+      <p class="welcome-message">欢迎！</p>
     </div>
   </div>
-
 </template>
 
 <script>
@@ -26,7 +13,6 @@
     name: 'main',
     data() {
       return {
-//        mainTabsVal: this.$store.state.mainTabsVal,
         //导航菜单是否展开
         spreadOrnot: this.$store.state.spread,
         mainTabs: this.$store.state.mainTabs,
@@ -39,16 +25,7 @@
     },
     methods: {
       removeTab(targetName) {
-        let tabs = this.mainTabs;
-        tabs.forEach((tab, index) => {
-          if (tab.name === targetName) {
-            let nextTab = tabs[index + 1] || tabs[index - 1];
-            store.commit("removeMainTabs",tab.name);
-            if (nextTab) {
-              store.commit("changeMainTabVal",nextTab.name);
-            }
-          }
-        });
+
       }
     }
   }
